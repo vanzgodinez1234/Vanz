3e6b4a8c4869da1abdea748954ffae9962ce99e6
vanzgodinez1234 <35475827+vanzgodinez1234@users.noreply.github.com>
2018-02-05 06:49:37 -0800

    point directly to root

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  <html> 
  <head> 
  	     <title>workshop 2</title> 
- 	     <link rel="stylesheet" type="text/css" href="../vannette/css/style.css"> 
+ 	     <link rel="stylesheet" type="text/css" href="../css/style.css"> 
  </head> 
  <body> 
  	  <div class="mainBase">
@@ -12,57 +12,57 @@
  	  	        </div>
  	  	        <div>
  	  	        	<h3>Router</h3>
- 	  	        	<img src="../vannette/images/router1.jpg">
+ 	  	        	<img src="../images/router1.jpg">
  	  	        	<p> 
  	  	        		A router connects networks. Based on its current understanding of the state of the network it is connected to, a router acts as a dispatcher as it decides which way to send each information packet. A router is located at any gateway (where one network meets another), including each point-of-presence on the internet. A router is often included as part of a network switch.
  	  	    		</p>
 
  	  	    		<h3>PC</h3>
- 	  	    		<img src="../vannette/images/pc.jpg">
+ 	  	    		<img src="../images/pc.jpg">
  	  	        	<p> 
  	  	        		A personal computer is a general-purpose, cost-effective computer that is designed to be used by a single end-user. Every PC is dependent on microprocessor technology, which allows PC makers to set the entire central processing unit (CPU) on a single chip.
  	  	    		</p>
 
  	  	    		<h3>RJ45</h3>
- 	  	    		<img src="../vannette/images/rg45.jpg">
+ 	  	    		<img src="../images/rg45.jpg">
  	  	        	<p> 
  	  	        		RJ45 is a type of connector commonly used for Ethernet networking. It looks similar to a telephone jack, but is slightly wider. Since Ethernet cables have an RJ45 connector on each end, Ethernet cables are sometimes also called RJ45 cables. 
  	  	    		</p>
 
  	  	    		<h3>Modem</h3>
- 	  	    		<img src="../vannette/images/modem.png">
+ 	  	    		<img src="../images/modem.png">
  	  	        	<p> 
  	  	        		Modem is a hardware device that allows a computer to send and receive data over a telephone line or a cable or satellite connection. In the case of transmission over an analog telephone line, which was once the most popular way to access the internet, the modem converts data between analog and digital formats in real time for two-way network communication. In the case of the high-speed digital modems popular today, the signal is much simpler and doesn't require the analog-to-digital conversion. 
 
  	  	    		</p>
 
  	  	    		<h3>Reapeter</h3>
- 	  	    		<img src="../vannette/images/repeater.jpg">
+ 	  	    		<img src="../images/repeater.jpg">
  	  	        	<p> 
  	  	        		In digital communication systems, a repeater is a device that receives a digital signal on an electromagnetic or optical transmission medium and regenerates the signal along the next leg of the medium. In electromagnetic media, repeaters overcome the attenuation caused by free-space electromagnetic-field divergence or cable loss. A series of repeaters make possible the extension of a signal over a distance.
  	  	    		</p>
 
  	  	    		<h3>Hub</h3>
- 	  	    		<img src="../vannette/images/hub.jpg">
+ 	  	    		<img src="../images/hub.jpg">
  	  	        	<p> 
  	  	        		Hub, also called a network hub, is a common connection point for devices in a network. Hubs are devices commonly used to connect segments of a LAN. The hub contains multiple ports. When a packet arrives at one port, it is copied to the other ports so that all segments of the LAN can see all packets.
  	  	    		</p>
 
  	  	    		<h3>Switch</h3>
- 	  	    		<img src="../vannette/images/switch.jpg">
+ 	  	    		<img src="../images/switch.jpg">
  	  	        	<p> 
  	  	        		A network switch (also called switching hub, bridging hub, officially MAC bridge) is a computer networking device that connects devices together on a computer network by using packet switching to receive, process, and forward data to the destination device.  
  	  	    		</p>
 
  	  	    		<h3>Connectors</h3>
- 	  	    		<img src="../vannette/images/connectors.jpg">
+ 	  	    		<img src="../images/connectors.jpg">
  	  	        	<p> 
  	  	        		A connector is the part on a cable or an appliance that has female contact pins and is intended to be attached to the end of the flexible cable remote from the supply.
   
  	  	    		</p>
 
  	  	    		<h3>Crimper</h3>
- 	  	    		<img src="../vannette/images/crimper.jpg">
+ 	  	    		<img src="../images/crimper.jpg">
  	  	        	<p> 
  	  	        	    A crimping tool is a device used to conjoin two pieces of metal by deforming one or both of them in a way that causes them to hold each other. The result of the tool's work is called a crimp. A good example of crimping is the process of affixing a connector to the end of a cable. For instance, network cables and phone cables are created using a crimping tool (shown below) to join the RJ-45 and RJ-11 connectors to the both ends of either phone or CAT5 cable.  
  	  	    		</p>
